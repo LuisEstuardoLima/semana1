47f8017ea611a9f6ce1835d1b7f500c48cd79995
Luis Estuardo Lima <1210460@galileo.edu>
2026-02-21 19:13:33 -0600

    Configuración inicial Express

---
+++ src/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API de Control de Hábitos funcionando');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});

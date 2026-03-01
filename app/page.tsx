5ff32148314c9e93684a845418868cd64e779d10
Luis Estuardo Lima <1210460@galileo.edu>
2026-02-28 20:13:00 -0600

    Frontend Next.js semana2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+'use client';
 import Image from "next/image";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchHabitsThunk } from "@/features/habit/habitSlice";
+import type { AppState, AppDispatch } from "@/Redux/store";
 
 export default function Home() {
+  const dispatch = useDispatch<AppDispatch>();
+  const habits = useSelector((state: AppState) => state.habit.items);
+  useEffect(() => {
+    dispatch(fetchHabitsThunk());
+}, [dispatch]);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
